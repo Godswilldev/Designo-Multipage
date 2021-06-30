330a58c50031412084a790506a7f1229285a98b7
Godswill Edet <gimmex1@gmail.com>
2021-06-30 23:06:55 +0100

    add footer styles

---
+++ src/Components/Footer/FooterStyles.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import * as variables from "../../Helpers/Variables";
+import { device } from "../../Helpers/MediaQueries";
+
+export const Footers = styled.footer`
+  .footer {
+    height: 39.3rem;
+    padding: 7rem;
+    background-color: ${variables.primaryColors.black};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    @media ${device.tablet} {
+      height: 100%;
+    }
+
+    .footer__nav {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 10rem;
+
+      .footerLogo {
+        width: 15rem;
+      }
+
+      @media ${device.tablet} {
+        flex-direction: column;
+        align-items: center;
+      }
+
+      .links {
+        text-decoration: none;
+        color: ${variables.primaryColors.white};
+        font-size: 1rem;
+        text-transform: uppercase;
+        &:not(:last-child) {
+          margin-right: 4rem;
+          @media ${device.tablet} {
+            margin-right: 0;
+          }
+        }
+
+        @media ${device.tablet} {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      }
+    }
+
+    .footer__details {
+      color: ${variables.primaryColors.white};
+      display: flex;
+      align-items: flex-end;
+      justify-content: space-between;
+      margin-top: 5rem;
+
+      &--links :not(:last-child) {
+        margin-right: 2rem;
+        cursor: pointer;
+      }
+
+      @media ${device.tablet} {
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        font-size: 1.5rem;
+        & > * {
+          margin-bottom: 5rem;
+        }
+      }
+    }
+  }
+`;
