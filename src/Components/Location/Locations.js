0b565a6995331fce84a644f94d488c2b2bc80f0c
Godswill Edet <gimmex1@gmail.com>
2021-07-09 09:54:38 +0100

    made locations page responsive

--- src/Components/Location/Locations.js
+++ src/Components/Location/Locations.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import canadaMap from "../../assets/locations/desktop/image-map-canada.png";
+import canadaMapTablet from "../../assets/locations/tablet/image-map-canada.png";
 import australiaMap from "../../assets/locations/desktop/image-map-australia.png";
+import australiaMapTablet from "../../assets/locations/tablet/image-map-australia.png";
 import ukMap from "../../assets/locations/desktop/image-map-united-kingdom.png";
+import ukMapTablet from "../../assets/locations/tablet/image-map-uk.png";
 import circles from "../../assets/shared/desktop/bg-pattern-two-circles.svg";
 import * as variables from "../../Helpers/Variables";
-
+import { device } from "../../Helpers/MediaQueries";
 const StyledLocations = styled.div`
   .location {
     margin: 5rem 0;
@@ -15,20 +18,24 @@ const StyledLocations = styled.div`
     grid-template-columns: repeat(3, 1fr);
     grid-gap: 2rem;
     overflow: hidden;
+    @media ${device.tablet} {
+      grid-template-columns: 1fr;
+      grid-template-rows: repeat(6, 1fr);
+      height: 100%;
+    }
 
     &__details {
       display: flex;
       justify-content: space-between;
-    }
 
-    .locations {
-      border-radius: 1rem;
-      background-color: #fdf3f0;
+      @media ${device.tablet} {
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
 
-      .map {
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
+        & > * {
+          margin-bottom: 3rem;
+        }
       }
     }
 
@@ -44,10 +51,17 @@ const StyledLocations = styled.div`
         position: absolute;
         bottom: 0;
         left: 0;
+        @media ${device.tablet} {
+          left: unset;
+          bottom: unset;
+        }
       }
       h1 {
         color: ${variables.primaryColors.peach};
         margin-bottom: 3rem;
+        @media ${device.tablet} {
+          text-align: center;
+        }
       }
     }
 
@@ -55,6 +69,15 @@ const StyledLocations = styled.div`
       grid-column: 1/3;
       &--map {
         background-image: url(${canadaMap});
+        @media ${device.tablet} {
+          background-image: url(${canadaMapTablet});
+          grid-column: 1/-1;
+          order: -6;
+        }
+      }
+      @media ${device.tablet} {
+        grid-column: unset;
+        order: -5;
       }
     }
 
@@ -63,12 +86,48 @@ const StyledLocations = styled.div`
 
       &--map {
         background-image: url(${australiaMap});
+        @media ${device.tablet} {
+          background-image: url(${australiaMapTablet});
+          grid-column: 1/-1;
+          order: -4;
+        }
+      }
+
+      @media ${device.tablet} {
+        grid-column: 1/-1;
+        order: -3;
       }
     }
     &__uk {
       grid-column: 1/3;
       &--map {
         background-image: url(${ukMap});
+        @media ${device.tablet} {
+          background-image: url(${ukMapTablet});
+          grid-column: 1/-1;
+          order: -2;
+        }
+      }
+      @media ${device.tablet} {
+        grid-column: 1/-1;
+      }
+    }
+
+    .locations {
+      border-radius: 1rem;
+      background-color: #fdf3f0;
+      @media ${device.tablet} {
+        padding: 5rem;
+      }
+
+      .map {
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        width: 100%;
+        @media ${device.tablet} {
+          /* order: -1; */
+        }
       }
     }
   }
